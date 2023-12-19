25679972484b97c5e397d64867c867ad27d89789
Geronimo Riesco <griesco@artech-consulting.com>
2023-12-19 10:57:32 -0300

    [data] Agrego objetos con la info

---
+++ src/data/cursos.js
@@ -0,0 +1,20 @@
+export const CURSOS = [
+    {
+        id: "1",
+        tecnologia: "SQL",
+        logo: "/sql.png",
+        drive: "https://drive.google.com/drive/folders/1kSskYQez3YVcM7pZSH6Y6yBV-GloaOro"
+    },
+    {
+        id: "2",
+        tecnologia: ".NET",
+        logo: "/dotnet.png",
+        drive: "https://drive.google.com/drive/folders/13bGEiGIdW3m41M8IG1Bvq08KrZoErI6T"
+    },
+    {
+        id: "3",
+        tecnologia: "React",
+        logo: "/react.svg",
+        drive: "https://drive.google.com/drive/folders/1ku8vJQersOSvk_ab1Pmrp83_ygubu1Xq"
+    }
+]
