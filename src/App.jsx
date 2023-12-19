88ce0f9823f1ed55e4c25abe9848cc55f5e68235
ZoeMlinarevic <Zoemmedl@gmail.com>
2023-12-19 12:16:50 -0300

    grupo 1 cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Navbar } from "./components/ui/Navbar";
+import { Cards} from "./components/ui/Cards";
 
 function App() {
   return (
-    <h1>Hola Mundo</h1>
+    <>
+    <Navbar/>
+    <div className="contenedor-tarjeta">
+    <Cards clases={9} tecnologia={"SQL"} profesor={"Leandro Morrone"} img={"sql.png"}/>
+    <Cards clases={6} tecnologia={"React"} profesor={"Ivan Carvajal - Geronimo Riesco"} img={"react.svg"}/>
+    <Cards clases={6} tecnologia={".NET"} profesor={"Leandro Morrone "} img={"dotnet.png"}/>
+    </div>
+    </>
   );
 }
 
