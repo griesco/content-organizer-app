1f6d8eedb8f801a44b02c1e6a0868c4a92e1d3e1
Geronimo Riesco <griesco@artech-consulting.com>
2023-12-27 15:29:33 -0300

    [Accordion] Pequeñas modificaciones

--- src/data/clases.js
+++ src/data/clases.js
@@ -3,7 +3,7 @@ export const CLASES = [
         idCurso: "1",
         id: "1",
         clase: "Clase 1 - Intro a redes y bases de datos",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1v-SUcFdutdLJk4BcFP7KIxLfbA75foHe",
         repositorio: null,
         video: null
@@ -12,7 +12,7 @@ export const CLASES = [
         idCurso: "1",
         id: "2",
         clase: "Clase 2 - Tablas y relaciones",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1Go7hPzDGuEnHNwDgPIGj8djc4It4uLHQ",
         repositorio: null,
         video: null
@@ -21,7 +21,7 @@ export const CLASES = [
         idCurso: "1",
         id: "3",
         clase: "Clase 3 - Normalización",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1ptTprlpfgTQNs8zJqKTfjoy9EAoc2D2C",
         repositorio: null,
         video: null
@@ -30,7 +30,7 @@ export const CLASES = [
         idCurso: "1",
         id: "4",
         clase: "Clase 4 - SQL Server y creación de BD",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1AZE6ebuzD96x6jtc2LX9jWGt2xZGAOL-",
         repositorio: null,
         video: null
@@ -39,7 +39,7 @@ export const CLASES = [
         idCurso: "1",
         id: "5",
         clase: "Clase 5 - INSERT, UPDATE, DELETE, SELECT",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1sVZm5VD5d6m2cuvrjHHTWKnSwaz33e72",
         repositorio: null,
         video: null
@@ -48,7 +48,7 @@ export const CLASES = [
         idCurso: "1",
         id: "6",
         clase: "Clase 6 - Más SELECT y JOINS",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1iuKYVrBCCMkFSRc945rERLdc3-UvSRGp",
         repositorio: null,
         video: null
@@ -57,7 +57,7 @@ export const CLASES = [
         idCurso: "1",
         id: "7",
         clase: "Clase 7 - Northwind - Selects y joins",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1xLIIAUmPhQQyXon_WmxlSjmDjJq4kxLD",
         repositorio: null,
         video: null
@@ -66,7 +66,7 @@ export const CLASES = [
         idCurso: "1",
         id: "8",
         clase: "Clase 8 - Northwind - Views, group by, union, SP",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1s2Pm2isngFyH3XyzSF70gCt29vv7-nnS",
         repositorio: null,
         video: null
@@ -75,7 +75,7 @@ export const CLASES = [
         idCurso: "1",
         id: "9",
         clase: "Clase 9 - Excel",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1_hxaGv9DPaUBQayjL4ti35hpoFhnr4fz",
         repositorio: null,
         video: null
@@ -84,7 +84,7 @@ export const CLASES = [
         idCurso: "2",
         id: "1",
         clase: "Clase 1 - Introducción a C#",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1o2H2rV9HKs9hZ4NsxN7xmFOt_ZkjDw_1",
         repositorio: "https://artech-gitlab.dyndns.org/users/lmorrone/projects",
         video: null
@@ -93,7 +93,7 @@ export const CLASES = [
         idCurso: "2",
         id: "2",
         clase: "Clase 2 - Introducción a POO",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1IGjtRvCL-57rIjBY3Xqd4OUtkXXJl9cL",
         repositorio: "https://artech-gitlab.dyndns.org/users/lmorrone/projects",
         video: null
@@ -102,7 +102,7 @@ export const CLASES = [
         idCurso: "2",
         id: "3",
         clase: "Clase 3 - Objetos y herencia",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1Snl92dGzB_rqRMOr7nssBbzAyeJzga8P",
         repositorio: "https://artech-gitlab.dyndns.org/users/lmorrone/projects",
         video: null
@@ -111,8 +111,8 @@ export const CLASES = [
         idCurso: "2",
         id: "4",
         clase: "Clase 4 - Ejercicio integrador POO",
-        resumen: "...",
-        material: "https://drive.google.com/drive/folders/1Snl92dGzB_rqRMOr7nssBbzAyeJzga8P",
+        resumen: "En proceso el resumen de la clase..",
+        material: "https://drive.google.com/drive/folders/1NmiE1c5znSg7rWtHDzATqBQO6Oqmh2c-",
         repositorio: "https://artech-gitlab.dyndns.org/users/lmorrone/projects",
         video: null
     },
@@ -120,8 +120,17 @@ export const CLASES = [
         idCurso: "2",
         id: "5",
         clase: "Clase 5 - ASP.Net API",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1-FIFAW4iORR1AwIwBmn658nXYmjICyIN",
+        repositorio: "https://artech-gitlab.dyndns.org/users/lmorrone/projects",
+        video: null
+    },
+    {
+        idCurso: "2",
+        id: "6",
+        clase: "Clase 6 - Entity Framework",
+        resumen: "En proceso el resumen de la clase..",
+        material: "https://drive.google.com/drive/folders/159TdeZLiA5rpUvqz2T2_hk2rCclZgnKP",
         repositorio: "https://artech-gitlab.dyndns.org/users/lmorrone/projects",
         video: null
     },
@@ -129,7 +138,7 @@ export const CLASES = [
         idCurso: "3",
         id: "1",
         clase: "Clase 1 - Introducción a JavaScript",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1JZExyiWde427O_vqWxRSP_s-dR0WHW64",
         repositorio: "https://artech-gitlab.dyndns.org/griesco/react-fisher",
         video: null
@@ -138,7 +147,7 @@ export const CLASES = [
         idCurso: "3",
         id: "2",
         clase: "Clase 2 - Introducción a React",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1Vh-tFkZhYBjAD10cVCS-30roMUyuUGZP",
         repositorio: "https://artech-gitlab.dyndns.org/griesco/react-fisher",
         video: null
@@ -147,7 +156,7 @@ export const CLASES = [
         idCurso: "3",
         id: "3",
         clase: "Clase 3 - Introducción a JSX",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/12l5bGjnrl0s3z80pkuwVJt7zA2hiCJu2",
         repositorio: "https://artech-gitlab.dyndns.org/griesco/react-fisher",
         video: null
@@ -156,7 +165,7 @@ export const CLASES = [
         idCurso: "3",
         id: "4",
         clase: "Clase 4 - Ejercicio Contador (useState)",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1sbnefV96g4TvLDJe2rj5ktUM3IYypo9V",
         repositorio: "https://artech-gitlab.dyndns.org/griesco/react-fisher",
         video: null
@@ -165,8 +174,17 @@ export const CLASES = [
         idCurso: "3",
         id: "5",
         clase: "Clase 5 - Ejercicio Integrador",
-        resumen: "...",
+        resumen: "En proceso el resumen de la clase..",
         material: "https://drive.google.com/drive/folders/1upFs1OXzFMSPsUicyQFs7Uo9VIt6ukcu",
+        repositorio: "https://artech-gitlab.dyndns.org/griesco/react-fisher",
+        video: null
+    },
+    {
+        idCurso: "3",
+        id: "6",
+        clase: "Clase 6 - Cursos de Artech | Website",
+        resumen: "En proceso el resumen de la clase..",
+        material: "https://drive.google.com/drive/folders/1zzP29YUiNz7NUe17hvr0X9N9YVId8Da_",
         repositorio: "https://artech-gitlab.dyndns.org/griesco/react-fisher",
         video: null
     }
